b4bfafa330c3e52054ffe7a1ae7212928c2fd187
Chris Chang <chrischang43@gmail.com>
2016-03-12 15:40:04 -0800

    adding gradient solution

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 
 var container = document.createElement("div");
 document.querySelector("body").appendChild(container);
-  for (var z = 0; z < 27; z++) {
+  var cellCount = 81;
+  var value = 255;
+  var step = 1/(cellCount / 9);
+  var opacity = 1;
+  for (var z = 1; z < cellCount + 1; z++) {
     var tiles = document.createElement("div");
-    if (z % 2 === 0) {
-      tiles.style.backgroundColor = "red";
-    } else {
-      tiles.style.backgroundColor = "green";
-    }
+      if (z % 2 === 0) {
+        tiles.style.background = `rgba(255,0,0,${opacity})`;
+      } else {
+        tiles.style.background = `rgba(0,0,255,${opacity})`;
+      }
     container.appendChild(tiles);
+    if (z % 9 === 0)
+    {
+      opacity = opacity - step;
+    }
   }
   document.querySelector("div").appendChild(tiles);  
