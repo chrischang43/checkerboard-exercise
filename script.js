fa925be7a9370a3fab6f436b87d29919a668283e
Chris Chang <chrischang43@gmail.com>
2016-03-12 14:33:00 -0800

    adding solution to randomColor

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 var container = document.createElement("div");
 document.querySelector("body").appendChild(container);
   for (var z = 0; z < 27; z++) {
+    var red = Math.floor(Math.random() * 255 + 1);
+    var green = Math.floor(Math.random() * 255 + 1);
+    var blue = Math.floor(Math.random() * 255 + 1);
     var tiles = document.createElement("div");
-    if (z % 2 === 0) {
-      tiles.style.backgroundColor = "red";
-    } else {
-      tiles.style.backgroundColor = "green";
-    }
+    tiles.style.backgroundColor = `rgb(${red},${green},${blue})`;
     container.appendChild(tiles);
   }
   document.querySelector("div").appendChild(tiles);  
