d70d29b211e4dc39d3ce3cf677cb145ab824bf97
Chris Chang <chrischang43@gmail.com>
2016-03-12 14:31:06 -0800

    adding checkboard solution

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your JS goes here
+
+var container = document.createElement("div");
+document.querySelector("body").appendChild(container);
+  for (var z = 0; z < 27; z++) {
+    var tiles = document.createElement("div");
+    if (z % 2 === 0) {
+      tiles.style.backgroundColor = "red";
+    } else {
+      tiles.style.backgroundColor = "green";
+    }
+    container.appendChild(tiles);
+  }
+  document.querySelector("div").appendChild(tiles);  
