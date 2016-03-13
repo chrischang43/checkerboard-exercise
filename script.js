284be8df14143d29a2eb25c339c21a70bf44975e
Chris Chang <chrischang43@gmail.com>
2016-03-12 16:11:19 -0800

    finishing flashing and the bonus

--- script.js
+++ script.js
@@ -3,12 +3,31 @@
 var container = document.createElement("div");
 document.querySelector("body").appendChild(container);
   for (var z = 0; z < 27; z++) {
+    var red = Math.floor(Math.random() * 255 + 1);
+    var green = Math.floor(Math.random() * 255 + 1);
+    var blue = Math.floor(Math.random() * 255 + 1);
     var tiles = document.createElement("div");
-    if (z % 2 === 0) {
-      tiles.style.backgroundColor = "red";
-    } else {
-      tiles.style.backgroundColor = "green";
-    }
+    tiles.style.backgroundColor = `rgb(${red},${green},${blue})`;
     container.appendChild(tiles);
   }
   document.querySelector("div").appendChild(tiles);  
+
+  var allDivs = document.querySelectorAll("div div");
+  function changing () {
+    for (var i = 0; i < allDivs.length; i++) {
+      var red = Math.floor(Math.random() * 255 + 1);
+      var green = Math.floor(Math.random() * 255 + 1);
+      var blue = Math.floor(Math.random() * 255 + 1);
+      var tiles = document.createElement("div");
+    allDivs[i].style.backgroundColor = `rgb(${red},${green},${blue})`;
+    }
+  }
+
+  setInterval(changing, 2000);
+  var audio = document.createElement("audio");
+  audio.setAttribute("src", "nyan.ogg");
+  audio.setAttribute("autoplay", "autoplay");
+  audio.setAttribute("loop", "loop");
+  audio.setAttribute("type", "audio/mp3");
+  document.querySelector("body").appendChild(audio);
+  
